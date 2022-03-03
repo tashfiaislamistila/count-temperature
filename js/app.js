9ad6aa20e9e7033f7723aa7c13caf9a8d63991d5
tashfiaislamistila <tashfia.cse@gmail.com>
2022-03-02 16:06:59 -0800

    commit

---
+++ js/app.js
@@ -0,0 +1,23 @@
+const API_key = `b4d1aae7970143e00b2db8794e5ca866`;
+const searchTemparature = () => {
+    const city = document.getElementById('city-name').value;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_key}&units=metric`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayTemperature(data));
+}
+
+const setInnerText = (id, text) => {
+    document.getElementById(id).innerText = text;
+}
+
+//for temparature display 
+const displayTemperature = temperature => {
+    setInnerText('city', temperature.name);
+    setInnerText('temperature', temperature.main.temp);
+    setInnerText('condition', temperature.weather[0].main);
+    //set weather icon
+    const url = `http://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`;
+    const imgIcon = document.getElementById('weather-icon');
+    imgIcon.setAttribute('src', url);
+}
